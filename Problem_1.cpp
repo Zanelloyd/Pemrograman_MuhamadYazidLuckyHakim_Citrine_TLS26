73bce612436140910319c69c67b2f0c08faf2b95
raghibakbars <raghib.as78@gmail.com>
2026-09-17 23:46:59 +0700

    Menambahkan putaran ke

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -7,15 +7,24 @@ int main(){
     cin >> N;
     cout << "Masukkan K: ";
     cin >> K;
+    cout << "\n";
     bool astronot[N];
     for(int i = 0; i < N; i++){
         astronot[i]=true;
     }
-    for(int i = 0; i < N; i++){
-        if(i % K == 0){
-        astronot[i] = false;
-        cout << "Astronot yang tereliminasi bernomor\t: " << i <<endl;
+    int putaran_ke = 1;
+    while(true){
+        if(i = K){
+            astronot[i] = false;
+            cout << "Putaran ke-" << putaran_ke << ",\nAstronot yang tereliminasi bernomor\t: " << i << endl;
+            if(i % 2 == 0){
+                K += 2;
+            } else {
+                K--;
+            }
+            cout << "K sekarang adalah\t\t\t: " << K << "\n\n";
+            putaran_ke ++;
+        }
+        return 0;
     }
-    }
-    return 0;
 }
