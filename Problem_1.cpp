fab95ffb95487c5b45d6c98bf1630eeaf7aef46b
Zane Lloyd <lloydzane33745@gmail.com>
2026-09-18 00:40:07 +0700

    Menambahkan pemutaran

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -9,20 +9,37 @@ int main(){
     cin >> K;
     cout << "\n";
     bool astronot[N];
+    
     for(int i = 0; i < N; i++){
         astronot[i]=true;
     }
+    
     int putaran_ke = 1;
-    if(i = K){
-        astronot[i] = false;
-        cout << "Putaran ke-" << putaran_ke << ",\nAstronot yang tereliminasi bernomor\t: " << i << endl;
-        if(i % 2 == 0){
-            K += 2;
-        } else {
-            K--;
+    int sisa = N;
+    int nomor = 0;
+
+     while(sisa > 1){
+        if(astronot[nomor] == true){
+            if(nomor % K == 0){
+                astronot[nomor] = false;
+                sisa--;
+                
+                cout << "Putaran ke-" << putaran_ke << endl << ", astronot bernomor\t: " << nomor << endl;
+                
+                if(nomor % 2 == 0){
+                    K += 2;
+                } else {
+                    K --;
+                }
+                cout << "K sekarang adalah\t\t\t: " << K << endl;
+                putaran_ke ++;
+            }
         }
-        cout << "K sekarang adalah\t\t\t: " << K << "\n\n";
-        putaran_ke ++;
+        
+        nomor = nomor + 1;
+        if(nomor > N){
+            nomor = 1;
+        }
     }
     return 0;
 }
