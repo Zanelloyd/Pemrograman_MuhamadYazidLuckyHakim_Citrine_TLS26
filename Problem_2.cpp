07ad1fd5e684837dde2398b9e9e8a193dc068670
Zane Lloyd <lloydzane33745@gmail.com>
2026-09-18 22:51:15 +0700

    logika awal

--- Problem_2.cpp
+++ Problem_2.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    char pesan[1000];
+    cin >> pesan;
+
+    int nilai_sebelum = 0;
+    
+    for (int i = 0; pesan[i] != '\0'; ++i) {
+        int nilai_semasa = pesan[i] - 'A' + 1;
+        
+        if (i == 0) {
+            cout << pesan[i];
+            nilai_sebelum = nilai_semasa;
+        } else {
+            int nilai_baru = nilai_semasa + nilai_sebelum;
+            int nilai_akhir = (nilai_baru - 1) % 26 + 1;
+            char aksara_baru = 'A' + nilai_akhir - 1;
+            
+            cout << aksara_baru;
+            nilai_sebelum = nilai_semasa;
+        }
+    }
+    
+  cout << '\n';
+    return 0;
+}
